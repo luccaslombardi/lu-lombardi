d3b57a73644ce76f8395918a72c3bbd6a8fd5d58
Dev Sweeteez <itech@sweeteez.com.br>
2022-05-23 11:26:29 -0300

    ajuste de posicionamento de botão + adição de animação de hover + adição de span superior + correção de overflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BsArrowDownShort } from "react-icons/bs";
 import { Header } from "./components/Header";
 import { Widget } from "./components/Widget";
 import { FirstBlock } from "./pages/FirstBlock";
@@ -7,10 +8,18 @@ export function App() {
   return (
     <>
       <Header />
-      <div className="flex h-screen w-screen bg-opacity-20 bg-indigo-900  backdrop-blur-sm backdrop-brightness-50">
+      <div className="flex h-screen w-screen bg-opacity-20 bg-indigo-900 justify-center backdrop-blur-sm backdrop-brightness-50">
         <FirstBlock />
+        <div className="absolute bottom-8 rounded-full border-2 text-4xl border-slate-300 mt-3 animate-bounce text-slate-300 hover:border-[#04D361] hover:text-[#04D361] transition-colors">
+          <a href="#secondBlock">
+            <BsArrowDownShort />
+          </a>
+        </div>
       </div>
-      <SecondBlock />
+      <div id="secondBlock">
+        <SecondBlock />
+      </div>
+
       <Widget />
     </>
   );
--- src/pages/FirstBlock.tsx
+++ src/pages/FirstBlock.tsx
@@ -3,27 +3,22 @@ import { BsArrowDownShort, BsGithub, BsInstagram, BsLinkedin, BsSpotify } from "
 export function FirstBlock() {
   return (
     <div className="container w-auto flex flex-col m-auto items-center drop-shadow-lg gap-3">
-      <div className=" bg-violet-900 bg-opacity-40 flex flex-col text-center items-center rounded-full p-8 z-10 drop-shadow-2xl">
+      <div className="-backdrop bg-violet-900 bg-opacity-40 flex flex-col text-center items-center rounded-full p-8 z-10 drop-shadow-2xl">
         <img
           src="https://media-exp1.licdn.com/dms/image/C4D03AQFYGCyOL7EDIw/profile-displayphoto-shrink_200_200/0/1642860856006?e=1658361600&v=beta&t=WjupcXbihaal7RIAdEap-_KCciQjVlqCu3cm8oJSzD4"
           alt="Luccas Lombardi"
-          className="rounded-full border-[20px] border-violet-800 w-[300px] md:w-[250px] md:border-[15px] "
-        />
+          className="rounded-full lg:border-[20px] border-violet-800 lg:w-[300px] md:w-[250px] md:border-[15px] " />
       </div>
-      <div className="w-96 bg-[#2f1456] flex flex-col text-center items-center rounded-2xl pt-48 pb-8 px-8 mt-[-11rem] md:w-[21rem]">
-        <h1 className="text-3xl md:text-2xl">LUCCAS LOMBARDI</h1>
-        <h3 className="text-lg md:text-base">Desenvolvedor Frontend</h3>
+      <div className="lg:w-96 bg-[#2f1456] flex flex-col text-center items-center rounded-2xl pt-48 pb-8 px-8 mt-[-12rem] md:w-[21rem]">
+          <span className="text-[#04D361] text-sm pb-1">HELLO WORLD!</span>
+        <h1 className="lg:text-3xl md:text-2xl">LUCCAS LOMBARDI</h1>
+        <h3 className="lg:text-lg md:text-base">Desenvolvedor Frontend</h3>
         <div className="flex justify-between text-center items-center gap-8 m-auto py-5">
-          <a href="https://instagram.com/luccas_lombardi" target="blank" className="hover:text-violet-800 transition-colors"><BsInstagram size={30} /></a>
-          <a href="https://linkedin.com/in/luccas-lombardi" target="blank" className="hover:text-violet-800 transition-colors"><BsLinkedin size={30} /></a>
-          <a href="https://github.com/luccaslombardi" target="blank" className="hover:text-violet-800 transition-colors"><BsGithub size={30} /></a>
-          <a href="https://open.spotify.com/user/22e533ad6qd7zck44ydhf2omi?si=96762ccd294445c0" target="blank" className="hover:text-violet-800 transition-colors"><BsSpotify size={30} /></a>
+          <a href="https://instagram.com/luccas_lombardi" target="blank" className="hover:mt-[-3px] hover:mb-[3px] ease-in-out duration-200"><BsInstagram size={30} /></a>
+          <a href="https://linkedin.com/in/luccas-lombardi" target="blank" className="hover:mt-[-3px] hover:mb-[3px] ease-in-out duration-200"><BsLinkedin size={30} /></a>
+          <a href="https://github.com/luccaslombardi" target="blank" className="hover:mt-[-3px] hover:mb-[3px] ease-in-out duration-200"><BsGithub size={30} /></a>
+          <a href="https://open.spotify.com/user/22e533ad6qd7zck44ydhf2omi?si=96762ccd294445c0" target="blank" className="hover:mt-[-3px] hover:mb-[3px] ease-in-out duration-200"><BsSpotify size={30} /></a>
         </div>
-      </div>
-      <div className="rounded-full border-2 text-4xl border-violet-800 mt-3 animate-bounce text-violet-800 hover:border-blue-800 hover:text-blue-800 transition-colors">
-        <a href="">
-          <BsArrowDownShort />
-        </a>
       </div>
     </div>
   );
