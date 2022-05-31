0c319f9aa7ded6895aa95e9cc87b9f989fd38d79
Dev Sweeteez <itech@sweeteez.com.br>
2022-05-31 12:01:52 -0300

    correção do erro no feedback

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "https://feedback-widget-backend-production-476a.up.railway.app"
 })
 
 export const apiSend = axios.create({
